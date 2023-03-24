ff3d77579f7e24cee5822074544fa10bf820312e
InhooPark <park.linhoo@gmail.com>
2023-03-24 13:40:10 +0900

    signout / main authorization

--- pages/src/Header.js
+++ pages/src/Header.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
-import hd from '@/styles/header.module.scss'
+import React from "react";
+import Link from "next/link";
+import hd from "@/styles/header.module.scss";
 import { useRouter } from "next/router";
-import { useRef, useEffect, useContext, useState } from 'react'
+import { useRef, useEffect, useContext, useState } from "react";
 import { DataContext } from "./MyContext";
+import { signOut, useSession } from "next-auth/react";
 
 function Header() {
   const audio = useRef();
-  const {who, pageChange, setClose, dataFun} = useContext(DataContext);
-  const [audioPlay,setAudio] = useState(false);
+  const { who, pageChange, setClose, dataFun } = useContext(DataContext);
+  const [audioPlay, setAudio] = useState(false);
   const router = useRouter();
+  const { status } = useSession();
 
-  
-  function togglePlay () {
+  function togglePlay() {
     if (!audio.current.paused) {
       audio.current.pause();
       setAudio(!audioPlay);
@@ -21,52 +22,41 @@ function Header() {
       setAudio(!audioPlay);
     }
   }
-  useEffect(() => {
-  }, []);
+  useEffect(() => {}, []);
 
-  function pageChangeHead(){
-    if(!pageChange){
-      setClose(true);
-      let closed;
-      closed = setTimeout(async function(){
-        setClose(!pageChange);
-        await dataFun("get");
-        router.push('/page/Main');
-      }, 1000);
-    }
-    // else if(!who){
-    //   console.log("진입 불가")
-    // }
-    else{
-      router.push('/page/Main');
+  function pageChangeHead() {
+    if (status === "unauthenticated") {
+      router.push("/");
+    } else {
+      if (!pageChange) {
+        setClose(true);
+        let closed;
+        closed = setTimeout(async function () {
+          setClose(!pageChange);
+          await dataFun("get");
+          router.push("/page/Main");
+        }, 1000);
+      } else {
+        router.push("/page/Main");
+      }
     }
   }
 
-
-
   return (
     <header className={hd.header}>
+      <div className={hd.music}>
+        <audio loop ref={audio} src="/audio/bonescatch.mp3"></audio>
+        <div className={`${hd.togglePlay} ${audioPlay && hd.active}`} onClick={togglePlay} />
+      </div>
 
-
-        <div className={hd.music}>
-          <audio loop ref={audio} src="/audio/bonescatch.mp3"></audio>
-          <div 
-            className={ `${hd.togglePlay} ${audioPlay && hd.active}`}
-            onClick={togglePlay}
-          />
-        </div>
-
-
-        <div className={hd.link} onClick={pageChangeHead}>
-            <img src='/img/logo0.png'></img>
-        </div>
-        <div className={hd.login}>
-          <Link href="/">로그아웃</Link>
-        </div>
-
-
+      <div className={hd.link} onClick={pageChangeHead}>
+        <img src="/img/logo0.png"></img>
+      </div>
+      <div className={hd.login}>
+        <button onClick={() => signOut()}>로그아웃</button>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
